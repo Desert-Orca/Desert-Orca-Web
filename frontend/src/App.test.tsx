73bb9316d465fa71f7a5a2b2ab34f7db5c49394c
Ani Chittabathini <aniruddhksai@gmail.com>
2024-04-07 21:38:41 -0400

    App Tests-Advanced

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -11,4 +11,13 @@ describe('The main app', () => {
     render(<App />);
     expect(screen.getByText('Payment')).toBeInTheDocument();
   });
+  it('should render the Home link', () => {
+    render(<App />);
+    // expect(secreen.getAllByText('Home')).toBeInTheDocument();
+    expect(screen.getByRole('link', {name:"Home"})).toBeInTheDocument();
+  });
+  it('should render the Home header', () => {
+    render(<App />);
+    expect(screen.getByRole('heading', { level:2 })).toBeInTheDocument();
+  });
 });
